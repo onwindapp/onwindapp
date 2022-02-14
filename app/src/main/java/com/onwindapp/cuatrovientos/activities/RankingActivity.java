2ae2df747fbb8e77b4aab7307c5b75db03c0c494
Miguel Puerta <migtarx@gmail.com>
2022-02-14 20:30:46 +0100

    feat: Added all related features to Ranking

--- app/src/main/java/com/onwindapp/cuatrovientos/activities/RankingActivity.java
+++ app/src/main/java/com/onwindapp/cuatrovientos/activities/RankingActivity.java
@@ -1,20 +1,44 @@
 package com.onwindapp.cuatrovientos.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import com.onwindapp.cuatrovientos.R;
+import com.onwindapp.cuatrovientos.adapters.RankingAdapter;
+import com.onwindapp.cuatrovientos.adapters.RidesAdapter;
+import com.onwindapp.cuatrovientos.models.Ride;
+import com.onwindapp.cuatrovientos.models.Users;
+
+import java.util.ArrayList;
+
+import io.realm.Realm;
+import io.realm.RealmResults;
 
 public class RankingActivity extends AppCompatActivity {
-
+    RankingAdapter rankingAdapter;
+    RecyclerView recyclerView;
+    RealmResults<Users> realmUsers;
+    LinearLayoutManager linearLayoutManager;
+    Realm realm;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ranking);
         getSupportActionBar().show();
 
+        realm = Realm.getDefaultInstance();
+        realmUsers = realm.where(Users.class).findAll();
 
-
-
+        rankingAdapter = new RankingAdapter(this, realmUsers);
+        recyclerView = findViewById(R.id.rankingRecycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setAdapter(rankingAdapter);
+        this.linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this,linearLayoutManager.getOrientation());
+        recyclerView.addItemDecoration(dividerItemDecoration);
     }
 }
--- app/src/main/java/com/onwindapp/cuatrovientos/adapters/RankingAdapter.java
+++ app/src/main/java/com/onwindapp/cuatrovientos/adapters/RankingAdapter.java
@@ -1,20 +1,29 @@
 package com.onwindapp.cuatrovientos.adapters;
 
 import android.content.Context;
+import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.onwindapp.cuatrovientos.R;
+import com.onwindapp.cuatrovientos.fragments.RidesAvailableFragment;
+import com.onwindapp.cuatrovientos.fragments.UserRidesFragment;
 import com.onwindapp.cuatrovientos.models.Ride;
 import com.onwindapp.cuatrovientos.models.Users;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RankingAdapter extends RecyclerView.Adapter<RankingAdapter.RankingViewHolder> {
     private List<Users> users;
@@ -30,13 +39,15 @@ public class RankingAdapter extends RecyclerView.Adapter<RankingAdapter.RankingV
     }
 
     public static class RankingViewHolder extends RecyclerView.ViewHolder {
-        public TextView username, date, tripType;
+        public TextView rankingDPosition, username, co2points;
+        public ImageView position;
 
         public RankingViewHolder(View itemView, final OnItemClickListener listener) {
             super(itemView);
-            username = (TextView) itemView.findViewById(R.id.driverNameUT);
-            date = (TextView) itemView.findViewById(R.id.date);
-            tripType = (TextView) itemView.findViewById(R.id.tripTypeUT);
+            username = (TextView) itemView.findViewById(R.id.rankingDriverName);
+            rankingDPosition = (TextView) itemView.findViewById(R.id.rankingDPosition);
+            co2points = (TextView) itemView.findViewById(R.id.co2points);
+            position = (ImageView) itemView.findViewById(R.id.rankingPosition);
             itemView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -51,26 +62,40 @@ public class RankingAdapter extends RecyclerView.Adapter<RankingAdapter.RankingV
         }
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     public RankingAdapter(Context context, List<Users> users) {
         this.context = context;
-        this.users = users;
+        this.users = users.stream().sorted(Comparator.comparing(Users::getCO2points).reversed()).collect(Collectors.toList());;
     }
 
 
     @Override
     public RankingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.user_ride_item, parent, false);
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.ranking_item, parent, false);
         RankingViewHolder evh = new RankingViewHolder(v, mListener);
         return evh;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public void onBindViewHolder(RankingViewHolder holder, int position) {
-        Users currentRide = users.get(position);
-
-        holder.username.setText(currentRide.getName());
-        holder.date.setText(currentRide.getMail());
-        holder.tripType.setText(Float.toString(currentRide.getCO2points()));
+        Users currentUser = users.get(position);
+        switch (position){
+            case 0:
+                holder.position.setImageResource(R.drawable.first);
+                break;
+            case 1:
+                holder.position.setImageResource(R.drawable.second);
+                break;
+            case 2:
+                holder.position.setImageResource(R.drawable.third);
+                break;
+            default:
+                holder.rankingDPosition.setBackgroundResource(R.drawable.round);
+                holder.rankingDPosition.setText(Integer.toString(position + 1));
+        }
+        holder.username.setText(currentUser.getName()+" "+currentUser.getSurname());
+        holder.co2points.setText(currentUser.getCO2points()+" co2 Points");
     }
 
     @Override
