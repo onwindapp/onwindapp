d885b7ca1ad4b79ed912936d1e36128394bdf703
Miguel Puerta <migtarx@gmail.com>
2022-02-15 23:54:44 +0100

    feat: Adding few changes to Ride Creation fragment 1 and fix to Ride Creation fragment 3

--- app/src/main/java/com/onwindapp/cuatrovientos/fragments/RideCreation1Fragment.java
+++ app/src/main/java/com/onwindapp/cuatrovientos/fragments/RideCreation1Fragment.java
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Switch;
 
 import com.onwindapp.cuatrovientos.R;
 
@@ -17,6 +18,7 @@ public class RideCreation1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ride_creation1, container, false);
+
         return view;
     }
 }
--- app/src/main/java/com/onwindapp/cuatrovientos/fragments/RideCreation3Fragment.java
+++ app/src/main/java/com/onwindapp/cuatrovientos/fragments/RideCreation3Fragment.java
@@ -15,12 +15,18 @@ import com.onwindapp.cuatrovientos.utils.CommonData;
 public class RideCreation3Fragment extends Fragment {
 
     public RideCreation3Fragment() {}
-
+    TextView txt3;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ride_creation3, container, false);
-        TextView txt3 = (TextView) view.findViewById(R.id.txt4);
-        txt3.setText(CommonData.selectedPosition.toString());
+        txt3 = (TextView) view.findViewById(R.id.txt4);
         return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        txt3.setText(CommonData.selectedPosition.toString());
+
     }
 }
