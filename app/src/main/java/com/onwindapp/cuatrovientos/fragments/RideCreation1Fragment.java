50f88dc8cbea484e65e42e793a285bf4657843b8
Miguel Puerta <migtarx@gmail.com>
2022-02-16 21:50:08 +0100

    feat: Fully built create form controls | bug: proxy error of realm object

--- app/src/main/java/com/onwindapp/cuatrovientos/fragments/RideCreation1Fragment.java
+++ app/src/main/java/com/onwindapp/cuatrovientos/fragments/RideCreation1Fragment.java
@@ -1,15 +1,26 @@
 package com.onwindapp.cuatrovientos.fragments;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.text.InputType;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Switch;
+import android.widget.TimePicker;
 
 import com.onwindapp.cuatrovientos.R;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class RideCreation1Fragment extends Fragment {
 
@@ -18,7 +29,64 @@ public class RideCreation1Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_ride_creation1, container, false);
+        Spinner seatsAvailable = (Spinner) view.findViewById(R.id.seatsAvailable);
+        EditText date, time;
+        time = (EditText) view.findViewById(R.id.timeSelector);
+        date = (EditText) view.findViewById(R.id.dateselector);
+        date.setInputType(InputType.TYPE_NULL);
+        time.setInputType(InputType.TYPE_NULL);
+
+        time.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showTimeDialog(time);
+            }
+        });
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDateDialog(date);
+            }
+        });
+        ArrayAdapter<String> seatsAvailableAdapter = new ArrayAdapter<String>(getActivity(),
+                android.R.layout.simple_expandable_list_item_1, getResources().getStringArray(R.array.seats));
+        seatsAvailableAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        seatsAvailableAdapter.notifyDataSetChanged();
+        seatsAvailable.setAdapter(seatsAvailableAdapter);
 
         return view;
+    }
+
+    private void showTimeDialog(final EditText time_in) {
+        final Calendar calendar= Calendar.getInstance();
+
+        TimePickerDialog.OnTimeSetListener timeSetListener=new TimePickerDialog.OnTimeSetListener() {
+            @Override
+            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                calendar.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                calendar.set(Calendar.MINUTE,minute);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm");
+                time_in.setText(simpleDateFormat.format(calendar.getTime()));
+            }
+        };
+
+        new TimePickerDialog(getActivity(),timeSetListener,calendar.get(Calendar.HOUR_OF_DAY),calendar.get(Calendar.MINUTE),true).show();
+    }
+
+
+    private void showDateDialog(final EditText date_in) {
+        final Calendar calendar=Calendar.getInstance();
+        DatePickerDialog.OnDateSetListener dateSetListener=new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR,year);
+                calendar.set(Calendar.MONTH,month);
+                calendar.set(Calendar.DAY_OF_MONTH,dayOfMonth);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                date_in.setText(simpleDateFormat.format(calendar.getTime()));
+
+            }
+        };
+        new DatePickerDialog(getActivity(),dateSetListener,calendar.get(Calendar.DAY_OF_MONTH),calendar.get(Calendar.MONTH),calendar.get(Calendar.YEAR)).show();
     }
 }
